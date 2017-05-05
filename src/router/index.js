1445fe344a9bd600c091811e80075203470ecc23
Brian Olore <brian@olore.net>
2017-05-05 07:24:30 -0400

    Add Investments & fonts, trial run for production deploy

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,11 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
 import Team from '@/components/Team';
+import Investments from '@/components/Investments';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -17,5 +18,16 @@ export default new Router({
       name: 'Team',
       component: Team,
     },
+    {
+      path: '/investments',
+      name: 'Investments',
+      component: Investments,
+    },
   ],
 });
+
+router.afterEach((to) => {
+  window.ga('send', 'pageview', to.path);
+});
+
+export default router;
