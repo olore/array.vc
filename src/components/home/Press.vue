60e203fdeb6df9c76c13ac0fce7722ba74a4f10c
Brian Olore <brian@olore.net>
2017-05-09 17:57:13 -0400

    fixup large images in Around The Globe

--- src/components/home/Press.vue
+++ src/components/home/Press.vue
@@ -20,29 +20,11 @@
         <div class="my-container">
 
 
-          <div style="float:left">
-            <div>
-              <a href="http://www.forbes.com/sites/valleyvoices/2015/01/26/when-tech-founders-should-talk-to-corpdev/"><img src="../../../static/img/press/forbes.png" style="padding-bottom: 10px; padding-left: 40px" alt="Forbes" /></a>
-            </div>
-            <div>
-              <a href="http://yourstory.com/2015/09/array-ventures-shruti-gandhi/"><img src="../../../static/img/press/yourstory.jpeg" alt="YourStory" /></a>
-            </div>
-          </div>
-
-          <div>
-            <a href="http://thepitchdeck.com/podcast/shruti-gandhi/"><img src="../../../static/img/press/the-pitchdeck-logo.png"  style="padding-left: 20px"/></a>
-          </div>
-
-
-          <div>
-            <div style="float:left">
-              <a href="http://venturebeat.com/author/shruti-gandhi-true-ventures/"><img src="../../../static/img/press/vb.png" alt="VentureBeat" style="padding-top: 10px"/></a>
-            </div>
-
-            <div>
-              <a href="http://www.usatoday.com/story/tech/columnist/2015/05/08/tech-bubble-winter-is-coming/26982203/"><img src="../../../static/img/press/usatoday.png" alt="USA Today" height="200"/></a>
-            </div>
-          </div>
+          <a href="http://www.forbes.com/sites/valleyvoices/2015/01/26/when-tech-founders-should-talk-to-corpdev/"><img src="../../../static/img/press/forbes.png" alt="Forbes" /></a>
+          <a href="http://yourstory.com/2015/09/array-ventures-shruti-gandhi/"><img src="../../../static/img/press/yourstory.jpeg" alt="YourStory" /></a>
+          <a href="http://thepitchdeck.com/podcast/shruti-gandhi/"><img src="../../../static/img/press/the-pitchdeck-logo.png" /></a>
+          <a href="http://venturebeat.com/author/shruti-gandhi-true-ventures/"><img src="../../../static/img/press/vb.png" alt="VentureBeat" /></a>
+          <a href="http://www.usatoday.com/story/tech/columnist/2015/05/08/tech-bubble-winter-is-coming/26982203/"><img src="../../../static/img/press/usatoday.png" alt="USA Today" /></a>
         </div>
 
       </div>
@@ -54,11 +36,6 @@
 
 export default {
   name: 'press',
-//  props: ['page'],
-//  data() {
-//    return {
-//    };
-//  },
 };
 
 </script>
@@ -68,8 +45,9 @@ export default {
   ul {
     list-style-type: none;
   }
-  li {
-
+  img {
+    max-width: 90%;
+    margin-bottom: 10px;
   }
 
   @media only screen
