4e83ec2df3e3e380f1f5ae3d30a9ffaf1c84514b
Brian Olore <brian@olore.net>
2017-11-17 17:17:53 -0500

    fix main nav links from advisors, remove hover underline and add color change

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,10 @@
 <template>
     <section class="nav">
       <router-link v-if="page !== 'home'" to="/">[ HOME ]</router-link>
-      <router-link v-if="page !== 'investments'" to="investments">[ INVESTMENTS ]</router-link>
-      <router-link v-if="page !== 'news'" to="news">[ NEWS ]</router-link>
-      <router-link v-if="page !== 'podcasts'" to="podcasts">[ PODCAST ]</router-link>
-      <router-link v-if="page !== 'advisors' && page !== 'team'" to="team">[ TEAM ]</router-link>
+      <router-link v-if="page !== 'investments'" to="/investments">[ INVESTMENTS ]</router-link>
+      <router-link v-if="page !== 'news'" to="/news">[ NEWS ]</router-link>
+      <router-link v-if="page !== 'podcasts'" to="/podcasts">[ PODCAST ]</router-link>
+      <router-link v-if="page !== 'advisors' && page !== 'team'" to="/team">[ TEAM ]</router-link>
     </section>
 </template>
 
@@ -28,7 +28,12 @@ export default {
   margin-bottom: 10px;
 }
 
-a {
+a, a:hover {
   white-space: nowrap;
+  text-decoration: none;
+}
+
+a:hover {
+  color: darkblue;
 }
 </style>
